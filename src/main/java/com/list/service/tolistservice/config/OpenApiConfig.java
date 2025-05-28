dbe956a60313b3d532d87ca436dbe3bb195876b8
AndruPudovkin <pudowkin98@mail.ru>
2025-05-28 20:50:17 +0300

    OpenApi not

--- src/main/java/com/list/service/tolistservice/config/OpenApiConfig.java
+++ src/main/java/com/list/service/tolistservice/config/OpenApiConfig.java
@@ -1,6 +1,7 @@
 package com.list.service.tolistservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,15 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info()
+                        .title("Your API Title")
+                        .version("1.0")
+                        .description("API Description"))
                 .addServersItem(new Server()
-                        .url("https://api.hfmf.ru")  // Основной продакшен URL
-                        .description("Production Server"))
+                        .url("https://api.hfmf.ru")
+                        .description("Production"))
                 .addServersItem(new Server()
-                        .url("http://localhost:8080")  // Локальный для разработки
-                        .description("Local Development"));
+                        .url("http://localhost:8080")
+                        .description("Local"));
     }
 }
