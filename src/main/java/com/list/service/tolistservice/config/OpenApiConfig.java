4d43962a92187390de0752c924569cba663eab22
AndruPudovkin <pudowkin98@mail.ru>
2025-05-28 21:10:07 +0300

    commit

--- src/main/java/com/list/service/tolistservice/config/OpenApiConfig.java
+++ src/main/java/com/list/service/tolistservice/config/OpenApiConfig.java
@@ -12,10 +12,6 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Your API Title")
-                        .version("1.0")
-                        .description("API Description"))
                 .addServersItem(new Server()
                         .url("https://api.hfmf.ru")
                         .description("Production"))
